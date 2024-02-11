8e9df0317946b4da5f4db68741c5ae12b9e5669c
Melissa M Kabalisa <kabalisamelissa2@gmail.com>
2024-02-11 22:36:31 +0200

    Create img2txt.py

---
+++ img2txt.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\mutes\AppData\Local\Programs\Python\Python311\Scripts\pytesseract.exe"
+# load image, grayscale, apply sharpening filter, otsu's threshold
+image = cv2.imread('C:\\Users\\mutes\\Desktop\\Melissa\\Notes\\Y3\\Emb-Sys\\T2\\water-meter-reading.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+# ocr
+data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
+# cv2.imshow('sharpen', sharpen)
+# cv2.imshow('thresh', thresh)
+# cv2.waitKey()
+numbers = ''.join(filter(str.isdigit, data))
+# Printing the  extracted numbers
+print("Extracted Numbers:", numbers)
